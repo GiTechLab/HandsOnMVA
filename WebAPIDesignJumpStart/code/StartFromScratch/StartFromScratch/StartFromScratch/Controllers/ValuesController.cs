53055339b5d77f5fc6f5cf135d2cecddcad215fb
Yang SHEN <bizbetter@outlook.com>
2015-11-02 00:22:37 +0800

    code start from scratch.

---
+++ WebAPIDesignJumpStart/code/StartFromScratch/StartFromScratch/StartFromScratch/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StartFromScratch.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] {"v1","v2" };
+        }
+
+        public string Get(int id)
+        {
+            return "valueT";
+        }
+    }
+}
---
+++ WebAPIDesignJumpStart/code/StartFromScratch/StartFromScratch/StartFromScratch/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StartFromScratch.WebApiApplication" Language="C#" %>
